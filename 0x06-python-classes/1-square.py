8221eab37caf689c05c7a561e9527b2dd0dc5cae
name <isaacodey01@gmail.com>
2023-06-15 13:37:15 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#Square with size by Isaac Odey
+"""Square Declaration"""
+
+
+class Square:
+    """Representing square"""
+
+    def __init__(self, size):
+    """Initializing the square
+    Args: size - represnets the size of the square defined
+    """
+    
+    self.__size=size
