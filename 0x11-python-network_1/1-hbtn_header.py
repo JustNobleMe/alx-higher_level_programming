50e8e6fe20f613d3f528a6237167c560a4bdc4e4
Noble <84409340+JustNobleMe@users.noreply.github.com>
2023-09-05 08:34:23 +0000

    htbn header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Script that takes URL, sends a request to the URL and displays the value of the X-Request-Id """
-
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
 import urllib.request
 
@@ -11,4 +12,3 @@ if __name__ == "__main__":
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
-
