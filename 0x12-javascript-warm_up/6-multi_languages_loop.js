0762754a936728797b02bbf21ecc0269b41aecc5
justnobleme <isaacodey01@gmail.com>
2023-11-14 13:55:56 -0800

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-myArray.forEach((element) => {
-	console.log(element);
+myVar.forEach((element) => {
+  console.log(element);
 });
